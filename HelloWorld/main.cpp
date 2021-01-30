128eb04dfaff081a6fd2ecdc0bfbe07d76ec34ef
yvettemuki <373628977@qq.com>
2021-01-30 15:40:06 +0800

    add: add EBO concept

--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -102,20 +102,36 @@ int main(void)
     glDeleteShader(fragmentShader);
 
     // set up vertex data (and buffer(s)) and configure vertex attributes
+//    // one triangle vertices
+//    float vertices[] = {
+//        -0.5f, -0.5f, 0.0f, // left
+//        0.5f, -0.5f, 0.0f, // right
+//        0.0f,  0.5f, 0.0f  // top
+//    };
+    // rectangle vertices
     float vertices[] = {
-        -0.5f, -0.5f, 0.0f, // left
-        0.5f, -0.5f, 0.0f, // right
-        0.0f,  0.5f, 0.0f  // top
+            0.5f, 0.5f, 0.0f,   // 右上角
+            0.5f, -0.5f, 0.0f,  // 右下角
+            -0.5f, -0.5f, 0.0f, // 左下角
+            -0.5f, 0.5f, 0.0f   // 左上角
+    };
+    unsigned int indices[] = { // 注意索引从0开始!
+            0, 1, 3, // 第一个三角形
+            1, 2, 3  // 第二个三角形
     };
 
-    unsigned int VBO, VAO;
+    unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO); //!!!!
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
     // bind the Vertex Array Object first, then bind and set vertex buffers(s), and then configure vertex attribute(s).
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0); //!!!!!
     glEnableVertexAttribArray(0); //!!!
@@ -131,6 +147,7 @@ int main(void)
 
     // uncomment this call to draw in wireframe polygons.
     //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
     // render loop
     while (!glfwWindowShouldClose(window))
@@ -146,7 +163,8 @@ int main(void)
         // draw our first triangle
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        // glDrawArrays(GL_TRIANGLES, 0, 3); // use vertex buffer to draw
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0); // use index buffer to draw
         // glBindVertexArray(0); // no need to unbind it every time
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
