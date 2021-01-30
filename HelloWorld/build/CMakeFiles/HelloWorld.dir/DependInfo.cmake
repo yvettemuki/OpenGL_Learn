e35c066232f9cae3ba255975e04b2db63c6e9adf
yvettemuki <373628977@qq.com>
2021-01-30 12:10:43 +0800

    initialize and add HelloWorld

---
+++ HelloWorld/build/CMakeFiles/HelloWorld.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/yvettemuki/Documents/code/OpenGL/HelloWorld/src/glad.c" "/Users/yvettemuki/Documents/code/OpenGL/HelloWorld/build/CMakeFiles/HelloWorld.dir/src/glad.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/Cellar/glew/2.1.0/include/GL"
+  "/usr/local/Cellar/glfw/3.2.1/include/GLFW"
+  "/usr/local/include/glad"
+  "/usr/local/include/KHR"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/yvettemuki/Documents/code/OpenGL/HelloWorld/main.cpp" "/Users/yvettemuki/Documents/code/OpenGL/HelloWorld/build/CMakeFiles/HelloWorld.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/Cellar/glew/2.1.0/include/GL"
+  "/usr/local/Cellar/glfw/3.2.1/include/GLFW"
+  "/usr/local/include/glad"
+  "/usr/local/include/KHR"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ HelloWorld/build/CMakeFiles/HelloWorld.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HelloWorld.dir/main.cpp.o"
+  "CMakeFiles/HelloWorld.dir/src/glad.c.o"
+  "HelloWorld"
+  "HelloWorld.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/HelloWorld.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
